b339141b614512be311a74c9066b684b72aef63c
juand-montiel <jdmz23481@gmail.com>
2026-03-05 18:38:41 -0600

    Update casona.js

--- pages/casona.js
+++ pages/casona.js
@@ -1,1 +1,17 @@
+export default function Casona() {
+  return (
+    <div>
+      <h1>Restaurante La Casona</h1>
+      <p>Dirección: Calle Central, Guápiles</p>
+      <p>Teléfono: 8888-8888</p>
+
+      <h2>Menú</h2>
+      <ul>
+        <li>Casado con pollo ₡3500</li>
+        <li>Hamburguesa ₡2800</li>
+        <li>Refresco natural ₡1200</li>
+      </ul>
+    </div>
+  );
+}
 
