1320e72efaa7b1dc20d82ff9b6d0ff1b19a55571
juand-montiel <jdmz23481@gmail.com>
2026-03-05 18:38:24 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Restaurantes de Guápiles</h1>
+      <ul>
+        <li><Link href="/casona">🍴 Restaurante La Casona</Link></li>
+        <li><Link href="/buen-sabor">🥪 Soda El Buen Sabor</Link></li>
+        <li><Link href="/delicias">🥖 Panadería Delicias</Link></li>
+      </ul>
+    </div>
+  );
+}
 
