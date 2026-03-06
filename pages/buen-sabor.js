955a198c333d13657b77fd803926b85e7216f947
juand-montiel <jdmz23481@gmail.com>
2026-03-05 18:38:56 -0600

    Update buen-sabor.js

--- pages/buen-sabor.js
+++ pages/buen-sabor.js
@@ -1,1 +1,17 @@
+export default function BuenSabor() {
+  return (
+    <div>
+      <h1>Soda El Buen Sabor</h1>
+      <p>Dirección: Avenida Principal, Guápiles</p>
+      <p>Teléfono: 8777-8777</p>
+
+      <h2>Menú</h2>
+      <ul>
+        <li>Gallo pinto ₡2500</li>
+        <li>Empanada ₡1200</li>
+        <li>Batido ₡1500</li>
+      </ul>
+    </div>
+  );
+}
 
