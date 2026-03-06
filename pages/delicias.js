8d1dc9d2622420334fe9aa7bc1a5b0e2af7591b6
juand-montiel <jdmz23481@gmail.com>
2026-03-05 18:39:12 -0600

    Update delicias.js

--- pages/delicias.js
+++ pages/delicias.js
@@ -1,1 +1,17 @@
+export default function Delicias() {
+  return (
+    <div>
+      <h1>Panadería Delicias</h1>
+      <p>Dirección: Barrio Centro, Guápiles</p>
+      <p>Teléfono: 8666-8666</p>
+
+      <h2>Productos</h2>
+      <ul>
+        <li>Pan dulce ₡800</li>
+        <li>Queque ₡2500</li>
+        <li>Rosquillas ₡1000</li>
+      </ul>
+    </div>
+  );
+}
 
